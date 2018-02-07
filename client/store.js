43a34953c44578d7b462d32e8ab4be0c11f8c21d
Neha <cneha88@gmail.com>
2018-02-07 17:15:52 -0500

    firstpart

--- client/store.js
+++ client/store.js
@@ -4,7 +4,7 @@ const GOT_MESSAGES_FROM_SERVER = 'GOT_MESSAGES_FROM_SERVER';
 const initialState = {
     messages: []
 };
-let store = createStore(todoApp, window.STATE_FR
+
 
 export const gotMessagesFromServer = function (messages){
     return {
@@ -22,5 +22,6 @@ function reducer (state = initialState, action) {
     }
 }
 
-
+let store = createStore(reducer);
+export default store;
 
